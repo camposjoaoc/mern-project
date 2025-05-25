106762d26e55d73a76aeadd942d15204db317433
João Canabarro Campos <joaocampos963@icloud.com>
2025-05-25 18:34:40 +0200

    Debugging itemController

--- server/src/controllers/itemController.js
+++ server/src/controllers/itemController.js
@@ -6,6 +6,7 @@ export const getAllItems = async (req, res) => {
         const items = await Item.find();
         res.json(items);
     } catch (error) {
+        console.error('Error fetching items:', error);
         res.status(500).json({ message: 'Error fetching items!' });
     }
 };
